46baed7ab6f2a56a4486d2917f6c3173922aa4d0
Juan <jc_espinozarodriguez@hotmail.com>
2024-10-21 16:10:18 -0600

    Se añadieron interacciones en la consola

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,16 @@ import './TodoSearch'
 
 function TodoSearch() {
     return (
-        <input placeholder="Cortar cebolla" />
+        <input placeholder="Cortar cebolla" 
+        className='TodoSearch'
+        onChange={(event) => {
+                console.log("Escritura en el TodoSearch");
+                console.log(event);
+                console.log(event.target);
+                console.log(event.target.value);
+            }
+        }
+        />
     )
 }
 
