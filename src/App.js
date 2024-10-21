5aab07ce0c624aea82822b421691622982720d74
Juan <jc_espinozarodriguez@hotmail.com>
2024-10-21 17:13:01 -0600

    Se añadió la funcionalidad de completar y eliminar ToDos

--- src/App.js
+++ src/App.js
@@ -30,6 +30,24 @@ function App() {
     }
   );
 
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <React.Fragment>
       <TodoCounter 
@@ -47,6 +65,12 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => 
+              completeTodo(todo.text)
+            }
+            onDelete={() =>
+              deleteTodo
+            }
           />
         ))}
       </TodoList>
