5da706af9b6a1140c6d6720098a6a93f38dee80b
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-10-20 23:05:35 -0600

    Añadi el botón asi como hice más dinamicos los elementos

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+import React from "react";
 import "./App.css";
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso de intro a React.js', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'lALALALA', completed: false },
+]
 
 function App() {
   return (
-    <div className="App">
-      <TodoCounter />
+    <React.Fragment>
+      <TodoCounter completed={16} total={25}/>
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
-    </div>
+    </React.Fragment>
   );
 }
 
