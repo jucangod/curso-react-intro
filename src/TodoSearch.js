adbbdd48ca1dfdbf9f3261f85bbc2f4993946df3
Juan <jc_espinozarodriguez@hotmail.com>
2024-10-21 16:37:36 -0600

    Agregamos los primeros estados y actualizadores de estados

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,16 +1,19 @@
+import React from 'react';
 import './TodoSearch'
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
+    console.log(searchValue);
+
     return (
-        <input placeholder="Cortar cebolla" 
-        className='TodoSearch'
-        onChange={(event) => {
-                console.log("Escritura en el TodoSearch");
-                console.log(event);
-                console.log(event.target);
-                console.log(event.target.value);
-            }
-        }
+        <input 
+            placeholder="Cortar cebolla" 
+            className='TodoSearch'
+            value={searchValue}
+            onChange={(event) => {
+                    setSearchValue(event.target.value);
+            }}
         />
     )
 }
