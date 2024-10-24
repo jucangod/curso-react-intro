315f472368d5df2c58daea2a7bb86005cada335c
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-10-23 20:42:08 -0600

    Se añadio la actualizacion de estados con useEffect, pero aun hay un error que provoca que la aplicacion no carge

--- src/App/index.js
+++ src/App/index.js
@@ -1,60 +1,54 @@
 import React from "react";
 import { useLocalStorage } from "./useLocalStorage";
-import { AppUI } from "./AppUi";
+import { AppUI } from "./AppUI";
 
 function App() {
-  const {
-    item: todos,
-    saveItem: saveTodos, 
-    loading,
-    error
-  } = useLocalStorage('', []);
-  const [searchValue, setSearchValue] = React.useState('');
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+    } = useLocalStorage("", []);
+    const [searchValue, setSearchValue] = React.useState("");
 
-  const completedTodos = todos.filter(
-    todo => !!todo.completed
-  ).length;
-  const totalTodos = todos.length;
+    const completedTodos = todos.filter(
+        (todo) => !!todo.completed
+    ).length;
+    const totalTodos = todos.length;
 
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
-    }
-  );
+    const searchedTodos = todos.filter((todo) => {
+        const todoText = todo.text.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+        return todoText.includes(searchText);
+    });
 
-  const completeTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+    const completeTodo = (text) => {
+        const newTodos = [...todos];
+        const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+        newTodos[todoIndex].completed = true;
+        saveTodos(newTodos);
+    };
+
+    const deleteTodo = (text) => {
+        const newTodos = [...todos];
+        const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+        newTodos.splice(todoIndex, 1);
+        saveTodos(newTodos);
+    };
+
+    return (
+        <AppUI
+        loading={loading}
+        error={error}
+        completedTodos={completedTodos}
+        totalTodos={totalTodos}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        searchedTodos={searchedTodos}
+        completeTodo={completeTodo}
+        deleteTodo={deleteTodo}
+        />
     );
-    newTodos[todoIndex].completed = true;
-    saveTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
-    );
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  };
-
-  return(
-    <AppUI
-    loading={loading}
-    error={error}
-    completedTodos={completedTodos}
-    totalTodos={totalTodos}
-    searchValue={searchValue}
-    setSearchValue={setSearchValue}
-    searchedTodos={searchedTodos}
-    completeTodo={completeTodo}
-    deleteTodo={deleteTodo}
-    />
-  )
 }
 
 export default App;
--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -6,22 +6,30 @@ function useLocalStorage(itemName, initialValue) {
     const [error, setError] = React.useState(false);
 
     React.useEffect(() => {
-        const localStorageItem = localStorage.getItem(itemName);
-        let parsedItem;
-
-        if (!localStorageTodos) {
-            localStorageItem.setItem('', JSON.stringify(initialValue));
-            parsedItem = [];
-          } else {
-            parsedItem = JSON.parse(localStorageItem);
-          }
+        setTimeout(() =>{
+            try {
+                const localStorageItem = localStorage.getItem(itemName);
+                let parsedItem;
+        
+                if (!localStorageItem) {
+                    localStorageItem.setItem('', JSON.stringify(initialValue));
+                    parsedItem = initialValue;
+                  } else {
+                    parsedItem = JSON.parse(localStorageItem);
+                    setItem(parsedItem);
+                  }
+        
+                  setLoading(false); 
+            } catch (error) {
+                setLoading(false);
+                setError(true);
+            }
+        }, 2000);
     });
-    
-    config [items, setItems] = React.useState(parsedItem);
   
     const saveItem = (newItem) => {
-      localStorage.setItem('', JSON.stringify(newItem));
-      setItem(newItem);
+        localStorage.setItem('', JSON.stringify(newItem));
+        setItem(newItem);
     };
   
     return {
