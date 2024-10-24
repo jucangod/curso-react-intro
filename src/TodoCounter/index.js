1ba9dd68d5b4b6b1e652bce743ba8d5d3f5ffadd
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-10-23 21:42:23 -0600

    Implementacion de useContext en la aplicacion

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import './TodoCounter.css';
+import { TodoContext } from '../TodoContext';
 
-function TodoCounter({ completed, total }) {
+function TodoCounter() {
+    const {
+        completedTodos,
+        totalTodos,
+    } = React.useContext(TodoContext);
+
     return (
         <h1 className='TodoCounter'>
-            Has completado <span>{completed}</span> de <span>{total}</span> TODOS
+            Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> TODOS
         </h1>
     )
 }
